d3ad4b3eafe74f8a97658814c39d66374734dcde
MoriGM <30366747+MoriGM@users.noreply.github.com>
2025-08-17 12:04:41 +0200

    Modernize loop

--- cd/scramble.go
+++ cd/scramble.go
@@ -11,7 +11,7 @@ func init() {
 
 	for i := scsi.SECTOR_SYNC_SIZE; i < scsi.SECTOR_DATA_SIZE; i++ {
 		DescrambleTable[i] = uint8(shiftRegister)
-		for bit := 0; bit < 8; bit++ {
+		for range 8 {
 			carry := uint16((shiftRegister & 1) ^ (shiftRegister >> 1 & 1))
 			shiftRegister = (carry<<15 | shiftRegister) >> 1
 		}
