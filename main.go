d49b0d6279ea347dc9303f470324c8ad1556c426
MoriGM <30366747+MoriGM@users.noreply.github.com>
2025-08-18 17:35:54 +0200

    Remove code from main

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
 import (
-	"dreamdump/cd"
-	"dreamdump/cd/sections"
 	"dreamdump/cli"
-	"dreamdump/log"
-	"dreamdump/option"
 )
-
-func readDisc(option option.Option) {
-	for _, section := range sections.GetSectionMap(option) {
-		log.WriteLn("{} {}", section.StartSector, section.EndSector)
-	}
-	sector, _ := cd.ReadSector(&option, 0)
-	log.WriteCleanLine(sector)
-}
 
 func main() {
 	option := cli.SetupOptions()
