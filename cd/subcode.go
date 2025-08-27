fec32aa8d374d5d39de965077253d75b78a15c40
MoriGM <30366747+MoriGM@users.noreply.github.com>
2025-08-27 10:12:47 +0200

    Remove magic numbers

--- cd/subcode.go
+++ cd/subcode.go
@@ -10,9 +10,19 @@ const (
 	SUBCHANNEL_Q = 6
 )
 
+const (
+	Q_CHANNEL_TRACK_NUMBER = 1
+	Q_CHANNEL_INDEX_NUMBER = 2
+	Q_CHANNEL_MINUTE       = 7
+	Q_CHANNEL_SECOND       = 8
+	Q_CHANNEL_FRAME        = 9
+	Q_CHANNEL_CHECKSUM_MSB = 10
+	Q_CHANNEL_CHECKSUM_LSB = 11
+)
+
 func (sub *Subchannel) Parse(subcodes [scsi.SECTOR_SUB_SIZE]uint8) {
 	for i, subcode := range subcodes {
-		sub.Qchannel[(i-(i%8))/8] |= ((subcode >> SUBCHANNEL_Q) & 0x01) << (7 - (i % 8))
+		sub.Qchannel[(i-(i%8))/8] |= ((subcode >> SUBCHANNEL_Q) & 0b00000001) << (7 - (i % 8))
 	}
 }
 
@@ -21,15 +31,15 @@ func (sub *QChannel) LBA() int32 {
 }
 
 func (sub *QChannel) AbsolutLBA() int32 {
-	return (int32(bcd.ToUint8(sub[7])) * MSF_MINUTE) + (int32(bcd.ToUint8(sub[8])) * MSF_SECOND) + (int32(bcd.ToUint8(sub[9])))
+	return (int32(bcd.ToUint8(sub[Q_CHANNEL_MINUTE])) * MSF_MINUTE) + (int32(bcd.ToUint8(sub[Q_CHANNEL_SECOND])) * MSF_SECOND) + (int32(bcd.ToUint8(sub[Q_CHANNEL_FRAME])))
 }
 
 func (sub *QChannel) TrackNumber() uint8 {
-	return bcd.ToUint8(sub[1])
+	return bcd.ToUint8(sub[Q_CHANNEL_TRACK_NUMBER])
 }
 
 func (sub *QChannel) IndexNumber() uint8 {
-	return bcd.ToUint8(sub[2])
+	return bcd.ToUint8(sub[Q_CHANNEL_INDEX_NUMBER])
 }
 
 func (sub *QChannel) TrackType() uint8 {
@@ -41,5 +51,5 @@ func (sub *QChannel) TrackType() uint8 {
 }
 
 func (sub *QChannel) CheckParity() bool {
-	return crc16_ccitt.Calculate(sub[0:10]) == ((uint16(sub[10]) << 8) | uint16(sub[11]))
+	return crc16_ccitt.Calculate(sub[0:10]) == ((uint16(sub[Q_CHANNEL_CHECKSUM_MSB]) << 8) | uint16(sub[Q_CHANNEL_CHECKSUM_LSB]))
 }
