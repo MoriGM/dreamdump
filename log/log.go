58748737826c24b9e398645ca9156f7075ecbe31
MoriGM <30366747+MoriGM@users.noreply.github.com>
2025-09-07 16:33:28 +0200

    Add file logs

--- log/log.go
+++ log/log.go
@@ -2,36 +2,72 @@ package log
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
+	"dreamdump/option"
 )
 
-var clean_len = 0
+var (
+	clean_len = 0
+	logFile   *os.File
+)
 
 func cleanLine() {
-	fmt.Print("\r")
-	for i := 0; i < clean_len+1; i++ {
-		fmt.Print(" ")
-	}
-	fmt.Print("\r")
+	cleanText := "\r" + strings.Repeat(" ", clean_len) + "\r"
+	fmt.Print(cleanText)
 }
 
 func Print(a ...any) {
 	text := fmt.Sprint(a...)
 	fmt.Print(text)
 	clean_len = len(text)
+	if logFile != nil {
+		_, err := logFile.WriteString(text)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func PrintClean(a ...any) {
 	cleanLine()
-	Print(a...)
+	text := fmt.Sprint(a...)
+	fmt.Print(text)
 }
 
 func Println(a ...any) {
+	text := fmt.Sprint(a...)
+	text += "\n"
 	cleanLine()
-	fmt.Println(a...)
+	fmt.Print(text)
 	clean_len = 0
+	if logFile != nil {
+		_, err := logFile.WriteString(text)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func Printf(msg string, a ...any) {
+	text := fmt.Sprintf(msg, a...)
 	cleanLine()
-	clean_len, _ = fmt.Printf(msg, a...)
+	clean_len = len(text)
+	fmt.Print(text)
+	if logFile != nil {
+		_, err := logFile.WriteString(text)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func Setup(opt *option.Option) {
+	logFileName := opt.PathName + "/" + opt.ImageName + ".log"
+	file, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil {
+		panic(err)
+	}
+	logFile = file
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	option := cli.SetupOptions()
+	log.Setup(&option)
 	commandFound := cli.ExecuteCommand(&option)
 	if !commandFound {
 		log.Println(os.Args[0] + " <disc> [--drive= --sector-order= --image-path= --image-name= --speed= --read-offset= --cutoff=]")
