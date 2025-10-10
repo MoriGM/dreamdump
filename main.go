4b486a8bf9e88a1dbc440ca28e6b5776c842ab75
MoriGM <30366747+MoriGM@users.noreply.github.com>
2025-10-10 20:31:41 +0200

    Add version string

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 	"dreamdump/log"
 )
 
+const (
+	VERSION = "0.0.0-alpha"
+)
+
 func main() {
 	option := cli.SetupOptions()
 	log.Setup(&option)
+	log.Println("Version: " + VERSION)
 	commandFound := cli.ExecuteCommand(&option)
 	if !commandFound {
 		log.Println(os.Args[0] + " <disc> [--drive= --sector-order= --image-path= --image-name= --speed= --read-offset= --cutoff=]")
