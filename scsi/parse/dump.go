513b211ffe175d2a291cea5821619aa399cbab48
MoriGM <30366747+MoriGM@users.noreply.github.com>
2025-10-31 15:05:14 +0100

    Rename file

---
+++ scsi/parse/dump.go
@@ -0,0 +1,24 @@
+package parse
+
+import (
+	"bytes"
+	"encoding/hex"
+)
+
+func stringify(a, b byte) string {
+	return DumpHex(append([]byte{a}, b))
+}
+
+func DumpHex(data []byte) string {
+	var buf bytes.Buffer
+	var tmp [3]byte
+	for i := range data {
+		hex.Encode(tmp[:], data[i:i+1])
+		tmp[2] = ' '
+		_, err := buf.Write(tmp[:3])
+		if err != nil {
+			return ""
+		}
+	}
+	return buf.String()
+}
